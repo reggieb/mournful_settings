f52f9e6bb039158e449aa62cc96a237662f8696c
Rob Nichols <git@nicholshayes.co.uk>
2013-01-29 16:06:09 +0000

    Adds tests and functionality from self_assessment settings

--- lib/mournful_settings/setting.rb
+++ lib/mournful_settings/setting.rb
@@ -2,5 +2,34 @@
 module MournfulSettings
   class Setting < ActiveRecord::Base
     self.table_name = 'mournful_settings_settings'
+    
+    VALUE_TYPES = %w{text number decimal}
+
+
+    validates :value_type, :presence => true, :inclusion => {:in => VALUE_TYPES}
+    validates :value, :presence => true
+    validates :name, :uniqueness => true, :presence => true
+
+    def self.value_types
+      VALUE_TYPES
+    end
+
+    def self.for(name)
+      setting = find_by_name(name)
+      setting.value if setting
+    end
+
+    def value
+      if value_type.present?
+        case value_type.to_s
+          when 'number'
+            super.to_f
+          when 'decimal'
+            BigDecimal.new(super.to_s)
+          else
+            super
+        end
+      end
+    end
   end
 end
--- test/dummy/test/setting_test.rb
+++ test/dummy/test/setting_test.rb
@@ -3,11 +3,62 @@ require_relative '../../test_helper'
 class SettingTest < Test::Unit::TestCase
   
   def setup
-    @setting = Setting.create(:name => 'test', :value => 'foo', :value_type => 'text')
+      
+  end
+  
+  def teardown
+    Setting.delete_all
   end
   
   def test_inheritence
-    assert_kind_of(MournfulSettings::Setting, @setting)   
+    assert_kind_of(MournfulSettings::Setting, text_setting)   
+  end
+  
+  def test_number_value
+    assert_kind_of(Float, number_setting.value)
+  end
+
+  def test_text_value
+    assert_kind_of(String, text_setting.value)
+  end
+
+  def test_decimal_value
+    assert_kind_of(BigDecimal, decimal_setting.value)
+  end
+
+  def test_valid_types
+    Setting::VALUE_TYPES.each do |valid_type|
+      number_setting.value_type = valid_type
+      assert(number_setting.valid?, "number_setting should be valid when value_type = #{valid_type}")
+    end
+  end
+
+  def test_invalid_type
+    number_setting.value_type = 'invalid'
+    assert(number_setting.invalid?, "number_setting should be invalid")
+  end
+
+  def test_for
+    [number_setting, text_setting, decimal_setting].each do |setting|
+      assert_equal(setting.value, Setting.for(setting.name.to_sym))
+    end
+  end
+
+  def test_for_when_no_matching_setting
+    assert_nil(Setting.for(:nothing), "Should return nil when setting doesn't exist")
+  end
+  
+  private
+  def text_setting 
+    @text_setting ||= Setting.create(:name => 'text_setting', :value => 'foo', :value_type => 'text')
+  end
+  
+  def number_setting
+    @number_setting ||= Setting.create(:name => 'number_setting', :value => '1.33333333333333', :value_type => 'number')    
+  end
+  
+  def decimal_setting
+    @decimal_setting ||= Setting.create(:name => 'decimal_setting', :value => '4.55', :value_type => 'decimal')    
   end
   
   
